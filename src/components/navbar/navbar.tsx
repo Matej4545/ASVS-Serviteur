17633183e535eee7a073385535ed3eb80fb5d9f6
Matej Groman <matejg@kentico.com>
2024-10-15 10:59:05 +0200

    Show report on parameter instead of path (GH pages issue)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -29,7 +29,7 @@ function Navbar() {
               text="This will remove all current progress and you will need to start new project from scratch. Make sure you did download the progress before proceeding!"
               onSubmit={() => clearData()}
             />
-            <Tooltip text="Audit report (in new window)"><a href="report" target="_blank"><Scroll /></a></Tooltip>
+            <Tooltip text="Audit report"><a href="?report=true"><Scroll /></a></Tooltip>
             <div>
               <Tooltip text="Export as JSON">
                 <a
